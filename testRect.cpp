1a92846087f98010a26d67b39088df9fe0f208da
Pascal Monasse <pascal.monasse@enpc.fr>
2025-09-16 11:58:59 +0200

    test beyond 2x2 images: nx2

--- testRect.cpp
+++ testRect.cpp
@@ -1,10 +1,21 @@
 #include "cc.h"
 #include <string>
+#include <iostream>
 
 int main(int argc, char* argv[]) {
-    float v[4]={0,1,2,3};
-    for(int i=0; i<4 && i+1<argc; i++)
+    size_t w=argc/2, h=2; 
+    float* v = new float[w*h];
+    for(int i=0; i+1<argc; i++)
         v[i] = std::stod(argv[i+1]);
-    CC cc(v,2,2);
+    for(int i=argc; i<w*h; i++)
+        v[i] = 0;
+    std::cout << "Image dimension: " << w << 'x' << h << std::endl;
+    for(int i=0,k=0; i<h; i++) {
+        for(int j=0; j<w; j++, k++) {
+            std::cout << v[k] << ' ';
+        }
+        std::cout << std::endl;
+    }
+    CC cc(v,w,h);
     return 0;
 }
